79676e10ef3ba88837c009b2237d91ccdc0f31e6
CONS-IND <56571972+CONS-IND@users.noreply.github.com>
2020-03-05 04:01:15 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,17 +1,17 @@
 exports.config = {
     // bot owner discord id
-    OWNER_ID: "101041126537973760",
+    OWNER_ID: "459169498730332161",
 
     // bot prefix
-    PREFIX: ".",
+    PREFIX: "cons?",
 
     // channel id's to ignore
-    IGNORE_CHANNELS: ["397961894654246913"],
+    IGNORE_CHANNELS: [""],
 
     // where to send welcome message (guildID : channelID)
     WELCOME_MESSAGE_CHANNEL: {
-        "408735794094669824": "408737565298917397",
-        "617836255350358037": "617836256268779521"  // dog
-        //"144518146232025088": "193151740743385089"
+        "408735794094669824": "",
+        "617836255350358037": ""  // dog
+        //"144518146232025088": ""
     }
 };
